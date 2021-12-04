67251e80404c82b1144fef052ddbb4b68dffa67e
Imtiaz Adar <75721079+imtiazaadar@users.noreply.github.com>
2021-12-04 19:58:23 +0600

    Add files via upload

--- BFS.py
+++ BFS.py
@@ -4,33 +4,32 @@
 from collections import deque
 from collections import defaultdict
 
-class graph:
+class Graph:
     def __init__(self):
-        self.vertex = defaultdict(list)
+        self.graph = defaultdict(list)
 
     def addEdge(self, u, v):
-        self.vertex[u].append(v)
-        self.vertex[v].append(u)
+        self.graph[u].append(v)
 
     def BFS(self, source):
-        visited = [False] * (len(self.vertex))
+        visited = [False] * (len(self.graph))
         queue = deque([])
         visited[source] = True
         queue.append(source)
         while len(queue) != 0:
             node = queue.popleft()
             print(node, end=' ')
-            for i in self.vertex[node]:
+            for i in self.graph[node]:
                 if not visited[i]:
                     visited[i] = True
                     queue.append(i)
         print()
 
-g = graph()
+g = Graph()
 vertex = int(input('Enter how many vertex do you want to add : '))
 edges = int(input('Enter how many edges do you want to add : '))
 for _ in range(edges):
-    print('Add Edge : ')
+    print('Add Edge : ', end='')
     source, destination = map(int, input().split())
     g.addEdge(source, destination)
 # 0, 1  0, 2  1, 2  2, 0  2, 3  3, 3
---
+++ DFS.py
@@ -0,0 +1,36 @@
+# Imtiaz Adar
+# DFS Implementation
+
+from collections import defaultdict
+
+class Graph:
+    def __init__(self):
+        self.graph = defaultdict(list)
+
+    def addEdge(self, u, v):
+        self.graph[u].append(v)
+
+    def DfsUtil(self, visited, source):
+        visited.add(source)
+        print(source, end=' ')
+        for neighbour in self.graph[source]:
+            if neighbour not in visited:
+                self.DfsUtil(visited, neighbour)
+
+    def DFS(self):
+        visited = set()
+        for vertex in self.graph:
+            if vertex not in visited:
+                self.DfsUtil(visited, vertex)
+
+g = Graph()
+vertex = int(input('Enter how many vertex do you want to add : '))
+edges = int(input('Enter how many edges do you want to add : '))
+for _ in range(edges):
+    print('Add Edge : ', end='')
+    source, destination = map(int, input().split())
+    g.addEdge(source, destination)
+    #print()
+# 0, 1  0, 2  1, 2  2, 0  2, 3  3, 3
+print('\n##- DFS -##\n')
+g.DFS()
