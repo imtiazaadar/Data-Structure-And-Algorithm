c0b0973d5d1274f12886ece55e0b5c38f216c785
Imtiaz Adar <75721079+imtiazaadar@users.noreply.github.com>
2022-02-12 12:27:25 +0600

    Binary Tree

---
+++ leftViewOfBinaryTree.java
@@ -0,0 +1,98 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+import static java.lang.Math.*;
+/**
+ * @author: Imtiaz Adar
+ */
+public class leftViewOfBinaryTree {
+    static class Node{
+        Node left, right;
+        int data;
+        Node(int data){
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+    public static void leftView(Node root){
+        if(root == null)
+            return;
+        Queue<Node> q = new LinkedList<>();
+        q.add(root);
+        while(!q.isEmpty()){
+            int size = q.size();
+            for(int i = 0; i < size; i++){
+                Node temp = q.poll();
+                if(i == 0){
+                    System.out.print(temp.data + " ");
+                }
+                if(temp.left != null){
+                    q.add(temp.left);
+                }
+                if(temp.right != null){
+                    q.add(temp.right);
+                }
+            }
+        }
+    }
+    public static void main(String[] args) throws IOException {
+        InputStreamReader reader = new InputStreamReader(System.in);
+        BufferedReader br = new BufferedReader(reader);
+        StringTokenizer token = new StringTokenizer("");
+        InputReader scan = new InputReader(br, token);
+        Node root = new Node(10);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(7);
+        root.left.right = new Node(8);
+        root.right.right = new Node(15);
+        root.right.left = new Node(12);
+        root.right.right.left = new Node(14);
+        leftView(root);
+    }
+    static class InputReader {
+        private BufferedReader readfile;
+        private StringTokenizer token;
+    
+        InputReader(BufferedReader br, StringTokenizer st)
+        {
+            this.readfile = br;
+            this.token = st;
+        }
+        String next(){
+            while (!this.token.hasMoreTokens()){
+                try{
+                    token = new StringTokenizer(readfile.readLine());
+                }
+                catch (IOException e){
+                    e.printStackTrace();
+                }
+            }
+            return this.token.nextToken();
+        }
+        String nextLine() throws IOException {
+            return readfile.readLine();
+        }
+        int nextInt(){
+            return Integer.parseInt(next());
+        }
+        double nextDouble(){
+            return Double.parseDouble(next());
+        }
+        long nextLong(){
+            return Long.parseLong(next());
+        }
+        int[] readArray(int size) throws IOException {
+            int[] dp = new int[size];
+            token = new StringTokenizer(readfile.readLine());
+            for (int i = 0; i < size; i++) {
+                dp[i] = Integer.parseInt(token.nextToken());
+            }
+            return dp;
+        }
+    }
+}
---
+++ rightViewOfBinaryTree.java
@@ -0,0 +1,97 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+import static java.lang.Math.*;
+/**
+ * @author: Imtiaz Adar
+ */
+public class rightViewOfBinaryTree {
+    static class Node{
+        Node left, right;
+        int data;
+        Node(int data){
+            this.data = data;
+            this.left = this.right = null;
+        }
+    }
+    public static void rightView(Node root){
+        if(root == null)
+            return;
+        Queue<Node> q = new LinkedList<>();
+        q.add(root);
+        while(!q.isEmpty()){
+            int size = q.size();
+            for(int i = 0; i < size; i++){
+                Node temp = q.poll();
+                if(i == size - 1){
+                    System.out.print(temp.data + " ");
+                }
+                if(temp.left != null){
+                    q.add(temp.left);
+                }
+                if(temp.right != null){
+                    q.add(temp.right);
+                }
+            }
+        }
+    }
+    public static void main(String[] args) throws IOException {
+        InputStreamReader reader = new InputStreamReader(System.in);
+        BufferedReader br = new BufferedReader(reader);
+        StringTokenizer token = new StringTokenizer("");
+        InputReader scan = new InputReader(br, token);
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+        root.right.left = new Node(6);
+        root.right.right = new Node(7);
+        root.right.left.right = new Node(8);
+        rightView(root);
+    }
+    static class InputReader {
+        private BufferedReader readfile;
+        private StringTokenizer token;
+    
+        InputReader(BufferedReader br, StringTokenizer st)
+        {
+            this.readfile = br;
+            this.token = st;
+        }
+        String next(){
+            while (!this.token.hasMoreTokens()){
+                try{
+                    token = new StringTokenizer(readfile.readLine());
+                }
+                catch (IOException e){
+                    e.printStackTrace();
+                }
+            }
+            return this.token.nextToken();
+        }
+        String nextLine() throws IOException {
+            return readfile.readLine();
+        }
+        int nextInt(){
+            return Integer.parseInt(next());
+        }
+        double nextDouble(){
+            return Double.parseDouble(next());
+        }
+        long nextLong(){
+            return Long.parseLong(next());
+        }
+        int[] readArray(int size) throws IOException {
+            int[] dp = new int[size];
+            token = new StringTokenizer(readfile.readLine());
+            for (int i = 0; i < size; i++) {
+                dp[i] = Integer.parseInt(token.nextToken());
+            }
+            return dp;
+        }
+    }
+}
